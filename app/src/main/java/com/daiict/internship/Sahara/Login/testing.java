3b37652de577c26d422021d19bc14ad82485d89c
Dev-sky <201912032@daiict.ac.in>
2020-07-18 17:42:32 +0530

    Test File

---
+++ app/src/main/java/com/daiict/internship/Sahara/Login/testing.java
@@ -0,0 +1,12 @@
+package com.daiict.internship.Sahara.Login;
+
+import android.content.Context;
+
+import androidx.appcompat.widget.AppCompatSpinner;
+
+public class testing extends AppCompatSpinner {
+
+    public testing(Context context) {
+        super(context);
+    }
+}
