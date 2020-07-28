6d507591b5d4677496852e6dc24f9bef2ccc98be
AshutoshAgarwal <ashutoshagarwal100@gmail.com>
2020-07-28 18:57:37 +0530

    User Registration Task Completed with Firebase Integration.

---
+++ app/src/main/java/com/daiict/internship/Sahara/ModelData/NeedyModelData.java
@@ -0,0 +1,129 @@
+package com.daiict.internship.Sahara.ModelData;
+
+public class NeedyModelData {
+    private String userId;
+    private String userName;
+    private String emailId;
+    private String address;
+    private String dateOfBirth;
+    private String contactNo;
+    private String cityName;
+    private String membersCount;
+    private String gender;
+    private String createDate;
+    private String modifyDate;
+    private String userIsVerified;
+
+    public NeedyModelData () {}
+
+    public NeedyModelData(String userId, String userName, String emailId, String address, String dateOfBirth, String contactNo, String cityName, String membersCount, String gender, String createDate, String modifyDate, String userIsVerified) {
+        this.userId = userId;
+        this.userName = userName;
+        this.emailId = emailId;
+        this.address = address;
+        this.dateOfBirth = dateOfBirth;
+        this.contactNo = contactNo;
+        this.cityName = cityName;
+        this.membersCount = membersCount;
+        this.gender = gender;
+        this.createDate = createDate;
+        this.modifyDate = modifyDate;
+        this.userIsVerified = userIsVerified;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getContactNo() {
+        return contactNo;
+    }
+
+    public void setContactNo(String contactNo) {
+        this.contactNo = contactNo;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public String getMembersCount() {
+        return membersCount;
+    }
+
+    public void setMembersCount(String membersCount) {
+        this.membersCount = membersCount;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(String createDate) {
+        this.createDate = createDate;
+    }
+
+    public String getModifyDate() {
+        return modifyDate;
+    }
+
+    public void setModifyDate(String modifyDate) {
+        this.modifyDate = modifyDate;
+    }
+
+    public String getUserIsVerified() {
+        return userIsVerified;
+    }
+
+    public void setUserIsVerified(String userIsVerified) {
+        this.userIsVerified = userIsVerified;
+    }
+}
--- app/src/main/java/com/daiict/internship/Sahara/SignUp/SignUpPage3.java
+++ app/src/main/java/com/daiict/internship/Sahara/SignUp/SignUpPage3.java
@@ -1,5 +1,6 @@
 package com.daiict.internship.Sahara.SignUp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,19 +8,36 @@ import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.daiict.internship.Sahara.Login.Login;
 import com.daiict.internship.Sahara.LoginSignUPDashboard.LoginSignUpDashboard;
+import com.daiict.internship.Sahara.ModelData.DonoModelData;
+import com.daiict.internship.Sahara.ModelData.NGOModelData;
+import com.daiict.internship.Sahara.ModelData.NeedyModelData;
+import com.daiict.internship.Sahara.ModelData.VolunteerModelData;
 import com.daiict.internship.Sahara.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.snackbar.BaseTransientBottomBar;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Calendar;
 
 public class SignUpPage3 extends AppCompatActivity {
 
@@ -30,7 +48,11 @@ public class SignUpPage3 extends AppCompatActivity {
     Switch aSwitch_worksfor;
     TextView textView_cityErrorMsg;
     RadioGroup rgrp_gender, rgrp_volunteer;
+    Button btn_signup;
     LinearLayout linearLayout_volunteer_gender, linearLayout_volunteer_type;
+
+    private FirebaseAuth mAuth;
+    private DatabaseReference mReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,7 +65,7 @@ public class SignUpPage3 extends AppCompatActivity {
     public void backBtnSignUpT(View view) {
         Intent intent = new Intent(SignUpPage3.this, SignUpPage2.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        intent.putExtra("category",get_category);
+        intent.putExtra("category", get_category);
         startActivity(intent);
 
     }
@@ -53,15 +75,17 @@ public class SignUpPage3 extends AppCompatActivity {
         RadioButton selectedRadioButton;
 
         if (validateContact() && validateCitySpinner() && validateGender() && validateVolunteerType() && validateMemberCount()) {
+            btn_signup = findViewById(R.id.btn_signup_third_signup);
+            btn_signup.setEnabled(false);
             // SignUp Process Start
             String contact = editText_contact.getText().toString().trim();
             String cityName = spin_signup_third_city.getSelectedItem().toString();
             // Get Gender
-            selectedRadioButtonID =  rgrp_gender.getCheckedRadioButtonId();
+            selectedRadioButtonID = rgrp_gender.getCheckedRadioButtonId();
             selectedRadioButton = (rgrp_gender.getCheckedRadioButtonId() != -1) ? (RadioButton) findViewById(selectedRadioButtonID) : null;
             String gender = (selectedRadioButton != null) ? selectedRadioButton.getText().toString() : null;
             // Get Volunteer Type
-            selectedRadioButtonID =  rgrp_volunteer.getCheckedRadioButtonId();
+            selectedRadioButtonID = rgrp_volunteer.getCheckedRadioButtonId();
             selectedRadioButton = (rgrp_volunteer.getCheckedRadioButtonId() != -1) ? (RadioButton) findViewById(selectedRadioButtonID) : null;
             String volunteerType = (selectedRadioButton != null) ? selectedRadioButton.getText().toString() : null;
             String isWorkNgo = (aSwitch_worksfor.getVisibility() == View.VISIBLE) ? (aSwitch_worksfor.isChecked() ? "Yes" : "No") : null;
@@ -74,6 +98,8 @@ public class SignUpPage3 extends AppCompatActivity {
             SignUpSingle.getInstance().setIsWorkWithNgo(isWorkNgo);
             SignUpSingle.getInstance().setMembersCount(memberCount);
 
+            // If all things and Validation are Correct then now Create User Account
+            createUserAccount();
         }
     }
 
@@ -104,15 +130,18 @@ public class SignUpPage3 extends AppCompatActivity {
         } else {
             linearLayout_volunteer_gender.setVisibility(View.VISIBLE);
         }
+
+        mAuth = FirebaseAuth.getInstance();
+        mReference = FirebaseDatabase.getInstance().getReference();
     }
 
     private boolean validateContact() {
         /*
-        * Conact Number Validation
-        * Empty - NonEmpty Validation
-        * Contact No Length
-        * Numeric Characters Only
-        * */
+         * Conact Number Validation
+         * Empty - NonEmpty Validation
+         * Contact No Length
+         * Numeric Characters Only
+         * */
 
         String contact = editText_contact.getText().toString().trim();
 
@@ -132,9 +161,9 @@ public class SignUpPage3 extends AppCompatActivity {
 
     private boolean validateCitySpinner() {
         /*
-        * City Spinner Validation:
-        * To check whether the Selected Option is Valid or Not
-        * */
+         * City Spinner Validation:
+         * To check whether the Selected Option is Valid or Not
+         * */
 
         String cityName = spin_signup_third_city.getSelectedItem().toString();
 
@@ -148,7 +177,7 @@ public class SignUpPage3 extends AppCompatActivity {
     }
 
     private boolean validateGender() {
-        if ((!get_category.equalsIgnoreCase("ngo"))&& rgrp_gender.getCheckedRadioButtonId() == -1) {
+        if ((!get_category.equalsIgnoreCase("ngo")) && rgrp_gender.getCheckedRadioButtonId() == -1) {
             View rootView = getWindow().getDecorView().getRootView();
             Snackbar genderErrMsg = Snackbar.make(rootView, "Select any one Gender", Snackbar.LENGTH_SHORT);
             genderErrMsg.show();
@@ -171,10 +200,10 @@ public class SignUpPage3 extends AppCompatActivity {
 
     private boolean validateMemberCount() {
         /*
-        * NGO Members or Family Members Count
-        * Empty-NonEmpty Validation
-        * Numeric Validation
-        * */
+         * NGO Members or Family Members Count
+         * Empty-NonEmpty Validation
+         * Numeric Validation
+         * */
 
         String membersCount = editText_ngo_member.getText().toString().trim();
 
@@ -188,5 +217,230 @@ public class SignUpPage3 extends AppCompatActivity {
             return true;
         }
 
+    }
+
+    private void createUserAccount() {
+        // Using Firebase Authentication, We create User Account
+        String emailId = SignUpSingle.getInstance().getEmailID();
+        String password = SignUpSingle.getInstance().getAccPass();
+
+        if (!emailId.isEmpty() && !password.isEmpty()) {
+            // Create Account with Email and Password
+            mAuth.createUserWithEmailAndPassword(emailId, password)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // If Creation of Account is Successfull....
+                                Log.e("onComplete: ", "Account Creation Successfull");
+                                // Send Email Verification
+                                sendEmailVerification();
+                            } else {
+                                // If Task is not Successfull
+                                Log.e("onComplete: ", String.valueOf(task.getException()));
+                                btn_signup.setEnabled(true);
+                            }
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    // Purpose is to fetch the Exception where Account with Given Email is Already Exist
+                    if (e.toString().equals("com.google.firebase.auth.FirebaseAuthUserCollisionException: The email address is already in use by another account.")) {
+                        View rootView = getWindow().getDecorView().getRootView();
+                        Snackbar alredyInUseMsg = Snackbar.make(rootView, "Given Email Already in Use", Snackbar.LENGTH_SHORT);
+                        alredyInUseMsg.show();
+                        btn_signup.setEnabled(true);
+                    } else {
+                        Log.e("Authentication Error: ", e.toString() + "");
+                        btn_signup.setEnabled(true);
+                    }
+                }
+            });
+        } else {
+            Toast.makeText(this, "Error in Singleton Pattern", Toast.LENGTH_SHORT).show();
+            btn_signup.setEnabled(true);
+        }
+    }
+
+    private void sendEmailVerification() {
+        // Get Current User From Firebase Authentication...
+        FirebaseUser mUser = mAuth.getCurrentUser();
+
+        if (mUser != null) {
+            mUser.sendEmailVerification().addOnCompleteListener(this, new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull Task<Void> task) {
+                    if (task.isSuccessful()) {
+                        Log.e("onComplete: ", "Email Verification Sent!!!");
+                        // Now the Final Task is to Register Data
+                        registerData();
+                    }
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Log.e("Verification Error:", e.toString() + "");
+                    btn_signup.setEnabled(true);
+                }
+            });
+        }
+    }
+
+    private void registerData() {
+        String userID = (mAuth != null) ? mAuth.getUid() : null;
+        String name = SignUpSingle.getInstance().getFirstName() + " " + SignUpSingle.getInstance().getLastName();
+        String address = SignUpSingle.getInstance().getAddressLine1() + ", " + SignUpSingle.getInstance().getAddressLine2();
+        String createDate = Calendar.getInstance().getTime().toString();
+        String modifyDate = Calendar.getInstance().getTime().toString();
+        String userIsVerified = "No";
+
+        assert userID != null;
+
+        if (get_category.equalsIgnoreCase("donor")) {
+            DonoModelData modelData = new DonoModelData(userID,
+                    name,
+                    SignUpSingle.getInstance().getEmailID(),
+                    address,
+                    SignUpSingle.getInstance().getDateOfBirth_Establishment(),
+                    SignUpSingle.getInstance().getContactNo(),
+                    SignUpSingle.getInstance().getCityName(),
+                    SignUpSingle.getInstance().getGender(),
+                    createDate,
+                    modifyDate,
+                    userIsVerified);
+
+            mReference.child(get_category).child(userID).setValue(modelData).addOnCompleteListener(new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull Task<Void> task) {
+                    if (task.isSuccessful()) {
+                        View rootView = getWindow().getDecorView().getRootView();
+                        Log.e("onComplete: ", "Registration Successfully");
+                        mAuth.signOut();
+                        Snackbar successFullMsg = Snackbar.make(rootView, "Done. Please Verify Email Before Login.", Snackbar.LENGTH_LONG);
+                        successFullMsg.show();
+                        Intent intent = new Intent(SignUpPage3.this, Login.class);
+                        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                        startActivity(intent);
+                        finish();
+                    }
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Log.e("onFailure: ", "Registration Failed!!" + " " + e.toString());
+                    btn_signup.setEnabled(true);
+                }
+            });
+        } else if (get_category.equalsIgnoreCase("ngo")) {
+            NGOModelData modelData = new NGOModelData(userID,
+                    name,
+                    SignUpSingle.getInstance().getEmailID(),
+                    SignUpSingle.getInstance().getNgoName(),
+                    address,
+                    SignUpSingle.getInstance().getDateOfBirth_Establishment(),
+                    SignUpSingle.getInstance().getContactNo(),
+                    SignUpSingle.getInstance().getCityName(),
+                    SignUpSingle.getInstance().getMembersCount(),
+                    createDate,
+                    modifyDate,
+                    userIsVerified);
+
+            mReference.child(get_category).child(userID).setValue(modelData).addOnCompleteListener(new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull Task<Void> task) {
+                    if (task.isSuccessful()) {
+                        View rootView = getWindow().getDecorView().getRootView();
+                        Log.e("onComplete: ", "Registration Successfully");
+                        mAuth.signOut();
+                        Snackbar successFullMsg = Snackbar.make(rootView, "Done. Please Verify Email Before Login.", Snackbar.LENGTH_LONG);
+                        successFullMsg.show();
+                        Intent intent = new Intent(SignUpPage3.this, Login.class);
+                        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                        startActivity(intent);
+                        finish();
+                    }
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Log.e("onFailure: ", "Registration Failed!!" + " " + e.toString());
+                    btn_signup.setEnabled(true);
+                }
+            });
+        } else if (get_category.equalsIgnoreCase("volunteer")) {
+            VolunteerModelData modelData = new VolunteerModelData(userID,
+                    name,
+                    SignUpSingle.getInstance().getEmailID(),
+                    address,
+                    SignUpSingle.getInstance().getDateOfBirth_Establishment(),
+                    SignUpSingle.getInstance().getContactNo(),
+                    SignUpSingle.getInstance().getCityName(),
+                    SignUpSingle.getInstance().getIsWorkWithNgo(),
+                    SignUpSingle.getInstance().getGender(),
+                    SignUpSingle.getInstance().getVolunteerType(),
+                    createDate,
+                    modifyDate,
+                    userIsVerified);
+
+            mReference.child(get_category).child(userID).setValue(modelData).addOnCompleteListener(new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull Task<Void> task) {
+                    if (task.isSuccessful()) {
+                        View rootView = getWindow().getDecorView().getRootView();
+                        Log.e("onComplete: ", "Registration Successfully");
+                        mAuth.signOut();
+                        Snackbar successFullMsg = Snackbar.make(rootView, "Done. Please Verify Email Before Login.", Snackbar.LENGTH_LONG);
+                        successFullMsg.show();
+                        Intent intent = new Intent(SignUpPage3.this, Login.class);
+                        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                        startActivity(intent);
+                        finish();
+                    }
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Log.e("onFailure: ", "Registration Failed!!" + " " + e.toString());
+                    btn_signup.setEnabled(true);
+                }
+            });
+        } else if (get_category.equalsIgnoreCase("needy")){
+            NeedyModelData modelData = new NeedyModelData(
+                    userID,
+                    name,
+                    SignUpSingle.getInstance().getEmailID(),
+                    address,
+                    SignUpSingle.getInstance().getDateOfBirth_Establishment(),
+                    SignUpSingle.getInstance().getContactNo(),
+                    SignUpSingle.getInstance().getCityName(),
+                    SignUpSingle.getInstance().getMembersCount(),
+                    SignUpSingle.getInstance().getGender(),
+                    createDate,
+                    modifyDate,
+                    userIsVerified);
+
+            mReference.child(get_category).child(userID).setValue(modelData).addOnCompleteListener(new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull Task<Void> task) {
+                    if (task.isSuccessful()) {
+                        View rootView = getWindow().getDecorView().getRootView();
+                        Log.e("onComplete: ", "Registration Successfully");
+                        mAuth.signOut();
+                        Snackbar successFullMsg = Snackbar.make(rootView, "Done. Please Verify Email Before Login.", Snackbar.LENGTH_LONG);
+                        successFullMsg.show();
+                        Intent intent = new Intent(SignUpPage3.this, Login.class);
+                        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                        startActivity(intent);
+                        finish();
+                    }
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Log.e("onFailure: ", "Registration Failed!!" + " " + e.toString());
+                    btn_signup.setEnabled(true);
+                }
+            });
+        }
     }
 }
