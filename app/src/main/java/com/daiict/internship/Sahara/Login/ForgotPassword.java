15a30af303f408e514414a0ea2666c204d7f19cd
riddhi <riddhishah2222@gmail.com>
2020-08-15 20:46:08 -0800

    forgot password module done with email

--- app/src/main/java/com/daiict/internship/Sahara/Login/ForgotPassword.java
+++ app/src/main/java/com/daiict/internship/Sahara/Login/ForgotPassword.java
@@ -1,16 +1,84 @@
 package com.daiict.internship.Sahara.Login;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.daiict.internship.Sahara.R;
+import com.daiict.internship.Sahara.SignUp.SelectionCategory;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.SignInMethodQueryResult;
+import com.google.firebase.database.DatabaseReference;
 
 public class ForgotPassword extends AppCompatActivity {
+
+    private TextInputEditText email;
+    private Button next_btn;
+    String user_email;
+
+    FirebaseAuth auth;
+    DatabaseReference ref;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password);
+
+        email=findViewById(R.id.forget_pass_email);
+        next_btn=findViewById(R.id.next_btn);
+
+        auth=FirebaseAuth.getInstance();
+
     }
+
+public void verifyEmail_nextbtn(View view){
+        user_email=email.getText().toString().trim();
+        if(user_email.equals(""))
+        {
+            Toast.makeText(this,"Please Enter Registered E-mail ID",Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            auth.fetchSignInMethodsForEmail(email.getText().toString()).addOnCompleteListener(new OnCompleteListener<SignInMethodQueryResult>() {
+                @Override
+                public void onComplete(@NonNull Task<SignInMethodQueryResult> task) {
+                    if (task.getResult().getSignInMethods().size() == 0){
+                        Toast.makeText(ForgotPassword.this,"Email is not registered. Please Sign Up First!",Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(ForgotPassword.this, SelectionCategory.class);
+                        startActivity(intent);
+                    }else {
+                        Toast.makeText(ForgotPassword.this,"Email existed.",Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(ForgotPassword.this, SelectForgotPassMethod.class);
+                        intent.putExtra("registered_email",user_email);
+                        startActivity(intent);
+                    }
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    e.printStackTrace();
+                }
+            });
+
+        }
+
+}
+
+
+    public void back_btn(View view) {
+        Intent intent = new Intent(ForgotPassword.this, Login.class);
+        startActivity(intent);
+    }
+
+
 }
