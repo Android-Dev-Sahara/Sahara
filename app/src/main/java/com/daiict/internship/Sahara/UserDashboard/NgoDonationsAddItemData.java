0c0adb143a47d930625ea99ca748219663b849f8
Dev-sky <201912032@daiict.ac.in>
2020-09-02 22:20:41 +0530

    NGO Fragment Left Added

---
+++ app/src/main/java/com/daiict/internship/Sahara/UserDashboard/NgoDonationsAddItemData.java
@@ -0,0 +1,129 @@
+package com.daiict.internship.Sahara.UserDashboard;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Switch;
+import android.widget.TextView;
+
+import com.daiict.internship.Sahara.R;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+
+public class NgoDonationsAddItemData extends AppCompatActivity {
+
+    public ArrayList<AddItemDataClass> list;
+    public adapter adapter_bind;
+    public RecyclerView rview;
+    private TextView closebutton;
+    private Button donateButton;
+    private ImageView imageViewDelete;
+
+    private String fragmentName, pickUpTime, isRawFood;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ngo_donations_add_item_data);
+        fragmentName = getIntent().getStringExtra("Fragment");
+        //pickUpTime = getIntent().getStringExtra("pickUpValue");
+        //isRawFood = getIntent().getStringExtra("isRawFood");
+
+        Log.e("onCreate: ", "Pick Up Time: " + pickUpTime + " Raw Food? " + isRawFood);
+
+        list = new ArrayList<>();
+        list.clear();
+        //Hooks added for recyclerview
+        rview = findViewById(R.id.recyclerview_dataitemshow_ngo);
+
+        //SetLayout Error has been removed
+        rview.setLayoutManager(new LinearLayoutManager(NgoDonationsAddItemData.this));
+
+        adapter_bind = new adapter(this, list);
+        rview.setAdapter(adapter_bind);
+        popupWindowInput();
+
+        donateButton = findViewById(R.id.add_btn_donate_ngo);
+        if (list.size() != 0) {
+            donateButton.setVisibility(View.VISIBLE);
+        }
+
+    }
+
+    public void donateButtonClickedNgo(View view) {
+
+        //Next Page
+        Intent intent = new Intent(this, NgoSelectionLocationAddData.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        intent.putExtra("Fragment", fragmentName);
+        startActivity(intent);
+
+    }
+
+    public void backBtnAddNgoDataDisplay(View view) {
+        Intent intent = new Intent(this, BottomNavigationUsers.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        intent.putExtra("Fragment", fragmentName);
+        startActivity(intent);
+
+
+    }
+
+    public void addItemNgo(View view) {
+        popupWindowInput();
+
+    }
+    public void popupWindowInput() {
+        AlertDialog.Builder Builder = new AlertDialog.Builder(NgoDonationsAddItemData.this);
+        View view = getLayoutInflater().inflate(R.layout.popup_window_fragment_donor_add_item, null);
+        final EditText itemName = view.findViewById(R.id.adddonor_edit_itemname_popup_window);
+        final EditText noofPersons = view.findViewById(R.id.adddonor_edit_noofpersons_popup_window);
+        final EditText weight = view.findViewById(R.id.adddonor_edit_weightfood_popup_window);
+        final Switch spoilage = view.findViewById(R.id.adddonor_switch_spoiler_popup_window);
+        final TextView closebutton = view.findViewById(R.id.adddonor_btn_close);
+
+        imageViewDelete = view.findViewById(R.id.add_donor_card_delete);
+
+        final boolean spoil = spoilage.isChecked();
+        Button addBasket = (Button) view.findViewById(R.id.adddonor_btn_addbucket_popup_window);
+        Builder.setView(view);
+        final Dialog dialog = Builder.create();
+        dialog.show();
+        addBasket.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!itemName.getText().toString().isEmpty() && !noofPersons.getText().toString().isEmpty() && !weight.getText().toString().isEmpty()) {
+                    Snackbar.make(view, "Item Added Successfully", Snackbar.LENGTH_LONG).show();
+                    list.add(new AddItemDataClass(itemName.getText().toString(), Integer.parseInt(noofPersons.getText().toString()), Float.parseFloat(weight.getText().toString()), spoil));
+                    dialog.dismiss();
+                    if (list.size() != 0) {
+                        donateButton.setVisibility(View.VISIBLE);
+                    }
+                    adapter_bind.notifyDataSetChanged();
+
+                } else {
+                    Snackbar.make(view, "Error!! Please Enter Valid Details !! ", Snackbar.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        closebutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+    }
+}
--- app/src/main/java/com/daiict/internship/Sahara/UserDashboard/RecentDonationsNGOFragment.java
+++ app/src/main/java/com/daiict/internship/Sahara/UserDashboard/RecentDonationsNGOFragment.java
@@ -1,5 +1,6 @@
 package com.daiict.internship.Sahara.UserDashboard;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,60 +8,57 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Switch;
 
 import com.daiict.internship.Sahara.R;
+import com.google.android.material.snackbar.Snackbar;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link RecentDonationsNGOFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class RecentDonationsNGOFragment extends Fragment {
+    Switch switch_isRawFood;
+    Spinner spinner_pickUpTime;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    String pickUpTime, isRawFood;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public RecentDonationsNGOFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment RecentDonationsNGOFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static RecentDonationsNGOFragment newInstance(String param1, String param2) {
-        RecentDonationsNGOFragment fragment = new RecentDonationsNGOFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    View view;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_recent_donations_n_g_o, container, false);
+        view = inflater.inflate(R.layout.fragment_recent_donations_n_g_o, container, false);
+        switch_isRawFood = (Switch) view.findViewById(R.id.switchRawFood_ngo);
+        spinner_pickUpTime = (Spinner) view.findViewById(R.id.spin_donation_ngo);
+        Button button = (Button) view.findViewById(R.id.btn_additem_ngo);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), NgoDonationsAddItemData.class);
+
+                if (validatePickUpTime()) {
+                    pickUpTime = spinner_pickUpTime.getSelectedItem().toString();
+                    isRawFood = switch_isRawFood.isChecked() ? "true" : "false";
+
+                    intent.putExtra("Fragment", "recentdonationsngofragment");
+                    //intent.putExtra("pickUpValue", pickUpTime);
+                    //intent.putExtra("isRawFood", isRawFood);
+
+                    startActivity(intent);
+                }
+            }
+        });
+
+        return view;
+    }
+
+    private boolean validatePickUpTime() {
+        if (!spinner_pickUpTime.getSelectedItem().toString().equals("--Select an Option--")) {
+            return true;
+        } else {
+            Snackbar errorMsg = Snackbar.make(view, "Select Pick Time.", Snackbar.LENGTH_LONG);
+            errorMsg.show();
+            return false;
+        }
     }
 }
